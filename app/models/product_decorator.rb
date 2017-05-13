df8a4a1ad9db329c0dc9565f1687362516c8814b
Huy Nguyen <huyngvu@gmail.com>
2017-05-13 13:50:25 +0700

    for spree 3.2.0

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -3,5 +3,9 @@ module Spree
     delegate_belongs_to :master, :sale_price
     delegate_belongs_to :master, :sale_start_date
     delegate_belongs_to :master, :sale_end_date
+    #attr_accessor :sale_price, :sale_start_date, :sale_end_date
+    #delegate :sale_price, to: :master
+    #delegate :sale_start_date, to: :master
+    #delegate :sale_end_date, to: :master
   end
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,7 @@
 module Spree
   Product.class_eval do
     delegate_belongs_to :master, :sale_price
+
+    #delegate :sale_price, to: :master
   end
 end
